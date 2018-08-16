d0b69f92aabf0235bc959db9e26e126c40f1096b
Carlingr <kittyclubco@gmail.com>
2018-08-16 10:54:16 -0400

    Endings and Vowels
    Restructured endings and vowels into objects
    Removed Unused Ba

--- sketch.js
+++ sketch.js
@@ -58,19 +58,20 @@ var output = {
   }
 };
 
-var ActEnds = ["m", "s", "t", "mus", "tis", "nt"]
-var PassEnds = ["r", "ris", "tur", "mur", "mini", "ntur"];
-var futureVowels = ["a", "e", "e", "e", "e", "e"];
-var perfectEnds = ["i", "isti", "it", "imus", "istis", "erunt"];
+var endings = {
+  active: ["m", "s", "t", "mus", "tis", "nt"],
+  passive: ["r", "ris", "tur", "mur", "mini", "ntur"],
+  perfect: ["i", "isti", "it", "imus", "istis", "erunt"],
+  futureperfect: ["", "", "", "", "", ""],
+  pluperfect: ["", "", "", "", "", ""]
+};
 
-
-var futureperfectEnds = ["", "", "", "", "", ""];
-var pluperfectEnds = ["", "", "", "", "", ""];
 var EstConj = ["sum", "es", "est", "sumus", "estis", "sunt"];
 
-
-var vowels = ["e", "i", "i", "i", "i", ""];
-var ba = ["ba", "ba", "ba", "ba", "ba", "ba"]
+var vowels = {
+  main: ["e", "i", "i", "i", "i", ""],
+  future: ["a", "e", "e", "e", "e", "e"]
+};
 
 var txt;
 
@@ -85,69 +86,69 @@ function setup() {
   roots[0] = givens[0].slice(0, -1); //get the root by dropping the last char of the string.
 
   /*present.ent Active Indicative*/
-  output.indicative.present.active = join_parts(roots[0], vowels, ActEnds);
+  output.indicative.present.active = join_parts(roots[0], vowels.main, endings.active);
   output.indicative.present.active[0] = roots[0] + "o"; //deal with 1st sing cause it's weird
   /*present.ent Passive Indicative*/
-  output.indicative.present.passive = join_parts(roots[0], vowels, PassEnds);
+  output.indicative.present.passive = join_parts(roots[0], vowels.main, endings.passive);
 
   /*imperfect.erfect Active Indicative*/
-  output.indicative.imperfect.active = join_parts(roots[0], "ba", ActEnds);
+  output.indicative.imperfect.active = join_parts(roots[0], "ba", endings.active);
   /*imperfect.erfect Passive Indicative*/
-  output.indicative.imperfect.passive = join_parts(roots[0], "ba", PassEnds);
+  output.indicative.imperfect.passive = join_parts(roots[0], "ba", endings.passive);
 
   /*future.ure Active Indicative*/
-  output.indicative.future.active = join_parts(roots[0], futureVowels, ActEnds);
+  output.indicative.future.active = join_parts(roots[0], vowels.future, endings.active);
   /*future.ure Passive Indicative*/
-  output.indicative.future.passive = join_parts(roots[0], futureVowels, PassEnds);
+  output.indicative.future.passive = join_parts(roots[0], vowels.future, endings.passive);
 
   /*page2*/
   roots[2] = givens[2].slice(0, -1); //get the root by dropping the last char of the string.
   roots[3] = givens[3].slice(0, -2) + "i"; //get the root by dropping the last char of the string.
 
   /*perfect.ect Active Indicative*/
-  output.indicative.perfect.active = join_parts(roots[2], perfectEnds);
+  output.indicative.perfect.active = join_parts(roots[2], endings.perfect);
   /*perfect.ect Passive Indicative*/
   output.indicative.perfect.passive = conj_with_est(" ", EstConj);
 
   /*pluperfect.erfect.ect Active Indicative*/
-  pluperfectEnds = add_string_to_array("era", ActEnds);
-  output.indicative.pluperfect.active = join_parts(roots[2], "", pluperfectEnds);
+  endings.pluperfect = add_string_to_array("era", endings.active);
+  output.indicative.pluperfect.active = join_parts(roots[2], "", endings.pluperfect);
   /*pluperfect.erfect.ect Passive Indicative*/
-  output.indicative.pluperfect.passive = conj_with_est(" ", pluperfectEnds)
+  output.indicative.pluperfect.passive = conj_with_est(" ", endings.pluperfect)
 
   /*futurePerfect.erfect.ect Active Indicative*/
-  futureperfectEnds = add_string_to_array("eri", ActEnds)
-  output.indicative.futurePerfect.active = join_parts(roots[2], futureperfectEnds);
+  endings.futureperfect = add_string_to_array("eri", endings.active)
+  output.indicative.futurePerfect.active = join_parts(roots[2], endings.futureperfect);
   output.indicative.futurePerfect.active[0] = roots[2] + "ero"; //1st sing is annoying
   /*futurePerfect.erfect.ect Passive Indicative*/
-  output.indicative.futurePerfect.passive = conj_with_est(" ", futureperfectEnds)
+  output.indicative.futurePerfect.passive = conj_with_est(" ", endings.futureperfect)
 
   /*Indicative*/
   /*page3*/
   roots[1] = givens[1];
 
   /*present.ent Active Subjunctive*/
-  output.subjunctive.present.active = join_parts(roots[0], "e", ActEnds);
+  output.subjunctive.present.active = join_parts(roots[0], "e", endings.active);
   /*present.ent Passive Subjunctive*/
-  output.subjunctive.present.passive = join_parts(roots[0], "a", PassEnds);
+  output.subjunctive.present.passive = join_parts(roots[0], "a", endings.passive);
 
   /*imperfect.erfect Active Subjunctive*/
-  output.subjunctive.imperfect.active = join_parts(roots[1], ActEnds);
+  output.subjunctive.imperfect.active = join_parts(roots[1], endings.active);
   /*imperfect.erfect Passive Subjunctive*/
-  output.subjunctive.imperfect.passive = join_parts(roots[1], PassEnds);
+  output.subjunctive.imperfect.passive = join_parts(roots[1], endings.passive);
 
 
   /*perfect.ect Active Subjunctive*/
-  output.subjunctive.perfect.active = join_parts(roots[2], futureperfectEnds);
+  output.subjunctive.perfect.active = join_parts(roots[2], endings.futureperfect);
   output.subjunctive.perfect.active[0] = roots[2] + "erem"; //1st sing is annoying
   /*perfect.ect Passive Subjunctive*/
-  output.subjunctive.perfect.passive = conj_with_est(" si", ActEnds);
+  output.subjunctive.perfect.passive = conj_with_est(" si", endings.active);
 
   /*pluperfect.erfect.ect Active Subjunctive*/
-  pluperfectEnds = add_string_to_array("isse", ActEnds);
-  output.subjunctive.pluperfect.active = join_parts(roots[2], pluperfectEnds);
+  endings.pluperfect = add_string_to_array("isse", endings.active);
+  output.subjunctive.pluperfect.active = join_parts(roots[2], endings.pluperfect);
   /*pluperfect.erfect.ect Passive Subjunctive*/
-  output.subjunctive.pluperfect.passive = conj_with_est(" esse", ActEnds);
+  output.subjunctive.pluperfect.passive = conj_with_est(" esse", endings.active);
 
   /*Output the stuffs*/
   txt = JSON.stringify(output, null, 2);
