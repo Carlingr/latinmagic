f771e02da44c59e3ad1f65fa7c847eb952b47cb0
Carlingr <kittyclubco@gmail.com>
2018-08-16 10:43:35 -0400

    Restructure Output object
    Namespace of output object is now more objective.
    Removed Unused Variable

--- sketch.js
+++ sketch.js
@@ -2,46 +2,71 @@ var givens = ["instruo", "instruere", "instruxi", "instructus"];
 var roots = ["", "", "", ""];
 
 var output = {
-  PresActInd: ["", "", "", "", "", ""],
-  PresPassInd: ["", "", "", "", "", ""],
-  ImpActInd: ["", "", "", "", "", ""],
-  ImpPassInd: ["", "", "", "", "", ""],
-  FutActInd: ["", "", "", "", "", ""],
-  FutPassInd: ["", "", "", "", "", ""],
+  indicative: {
+    present: {
+      active: ["", "", "", "", "", ""],
+      passive: ["", "", "", "", "", ""],
+    },
+    imperfect: {
+      active: ["", "", "", "", "", ""],
+      passive: ["", "", "", "", "", ""],
+    },
+    future: {
+      active: ["", "", "", "", "", ""],
+      passive: ["", "", "", "", "", ""],
+    },
 
-  PerfActInd: ["", "", "", "", "", ""],
-  PerfPassInd: ["", "", "", "", "", ""],
-  PlupActInd: ["", "", "", "", "", ""],
-  PlupPassInd: ["", "", "", "", "", ""],
-  FutPActInd: ["", "", "", "", "", ""],
-  FutPPassInd: ["", "", "", "", "", ""],
+    perfect: {
+      active: ["", "", "", "", "", ""],
+      passive: ["", "", "", "", "", ""],
+    },
+    pluperfect: {
+      active: ["", "", "", "", "", ""],
+      passive: ["", "", "", "", "", ""],
+    },
+    futurePerfect: {
+      active: ["", "", "", "", "", ""],
+      passive: ["", "", "", "", "", ""],
+    },
+  },
+  subjunctive: {
+    present: {
+      active: ["", "", "", "", "", ""],
+      passive: ["", "", "", "", "", ""],
+    },
+    imperfect: {
+      active: ["", "", "", "", "", ""],
+      passive: ["", "", "", "", "", ""],
+    },
+    future: {
+      active: ["", "", "", "", "", ""],
+      passive: ["", "", "", "", "", ""],
+    },
 
-
-  PresActSubj: ["", "", "", "", "", ""],
-  PresPassSubj: ["", "", "", "", "", ""],
-  ImpActSubj: ["", "", "", "", "", ""],
-  ImpPassSubj: ["", "", "", "", "", ""],
-  FutActSubj: ["", "", "", "", "", ""],
-  FutPassSubj: ["", "", "", "", "", ""],
-
-  PerfActSubj: ["", "", "", "", "", ""],
-  PerfPassSubj: ["", "", "", "", "", ""],
-  PlupActSubj: ["", "", "", "", "", ""],
-  PlupPassSubj: ["", "", "", "", "", ""],
-  FutPActSubj: ["", "", "", "", "", ""],
-  FutPPassSubj: ["", "", "", "", "", ""],
+    perfect: {
+      active: ["", "", "", "", "", ""],
+      passive: ["", "", "", "", "", ""],
+    },
+    pluperfect: {
+      active: ["", "", "", "", "", ""],
+      passive: ["", "", "", "", "", ""],
+    },
+    futurePerfect: {
+      active: ["", "", "", "", "", ""],
+      passive: ["", "", "", "", "", ""],
+    }
+  }
 };
 
 var ActEnds = ["m", "s", "t", "mus", "tis", "nt"]
 var PassEnds = ["r", "ris", "tur", "mur", "mini", "ntur"];
-var FutVowels = ["a", "e", "e", "e", "e", "e"];
-var PerfEnds = ["i", "isti", "it", "imus", "istis", "erunt"];
+var futureVowels = ["a", "e", "e", "e", "e", "e"];
+var perfectEnds = ["i", "isti", "it", "imus", "istis", "erunt"];
 
 
-var FutPEnds = ["", "", "", "", "", ""];
-var PlupEnds = ["", "", "", "", "", ""];
+var futureperfectEnds = ["", "", "", "", "", ""];
+var pluperfectEnds = ["", "", "", "", "", ""];
 var EstConj = ["sum", "es", "est", "sumus", "estis", "sunt"];
-var blank_array = ["", "", "", "", "", ""];
 
 
 var vowels = ["e", "i", "i", "i", "i", ""];
@@ -50,85 +75,87 @@ var ba = ["ba", "ba", "ba", "ba", "ba", "ba"]
 var txt;
 
 function setup() {
-  var i;
-  for (i = 0; i < 4; i++) {
-    givens[i] = prompt("enter the " + ordinal_suffix_of(i + 1) + " principal part")
-  }
+  // var i;
+  // for (i = 0; i < 4; i++) {
+  //   givens[i] = prompt("enter the " + ordinal_suffix_of(i + 1) + " principal part")
+  // }
 
   /*Indicative*/
   /*page1*/
   roots[0] = givens[0].slice(0, -1); //get the root by dropping the last char of the string.
 
-  /*Present Active Indicative*/
-  output.PresActInd = join_parts(roots[0], vowels, ActEnds);
-  output.PresActInd[0] = roots[0] + "o"; //deal with 1st sing cause it's weird
-  /*Present Passive Indicative*/
-  output.PresPassInd = join_parts(roots[0], vowels, PassEnds);
+  /*present.ent Active Indicative*/
+  output.indicative.present.active = join_parts(roots[0], vowels, ActEnds);
+  output.indicative.present.active[0] = roots[0] + "o"; //deal with 1st sing cause it's weird
+  /*present.ent Passive Indicative*/
+  output.indicative.present.passive = join_parts(roots[0], vowels, PassEnds);
 
-  /*Imperfect Active Indicative*/
-  output.ImpActInd = join_parts(roots[0], "ba", ActEnds);
-  /*Imperfect Passive Indicative*/
-  output.ImpPassInd = join_parts(roots[0], "ba", PassEnds);
+  /*imperfect.erfect Active Indicative*/
+  output.indicative.imperfect.active = join_parts(roots[0], "ba", ActEnds);
+  /*imperfect.erfect Passive Indicative*/
+  output.indicative.imperfect.passive = join_parts(roots[0], "ba", PassEnds);
 
-  /*Future Active Indicative*/
-  output.FutActInd = join_parts(roots[0], FutVowels, ActEnds);
-  /*Future Passive Indicative*/
-  output.FutPassInd = join_parts(roots[0], FutVowels, PassEnds);
+  /*future.ure Active Indicative*/
+  output.indicative.future.active = join_parts(roots[0], futureVowels, ActEnds);
+  /*future.ure Passive Indicative*/
+  output.indicative.future.passive = join_parts(roots[0], futureVowels, PassEnds);
 
   /*page2*/
   roots[2] = givens[2].slice(0, -1); //get the root by dropping the last char of the string.
   roots[3] = givens[3].slice(0, -2) + "i"; //get the root by dropping the last char of the string.
 
-  /*Perfect Active Indicative*/
-  output.PerfActInd = join_parts(roots[2], PerfEnds);
-  /*Perfect Passive Indicative*/
-  output.PerfPassInd = conj_with_est(" ", EstConj);
+  /*perfect.ect Active Indicative*/
+  output.indicative.perfect.active = join_parts(roots[2], perfectEnds);
+  /*perfect.ect Passive Indicative*/
+  output.indicative.perfect.passive = conj_with_est(" ", EstConj);
 
-  /*Pluperfect Active Indicative*/
-  PlupEnds = add_string_to_array("era", ActEnds);
-  output.PlupActInd = join_parts(roots[2], "", PlupEnds);
-  /*Pluperfect Passive Indicative*/
-  output.PlupPassInd = conj_with_est(" ", PlupEnds)
+  /*pluperfect.erfect.ect Active Indicative*/
+  pluperfectEnds = add_string_to_array("era", ActEnds);
+  output.indicative.pluperfect.active = join_parts(roots[2], "", pluperfectEnds);
+  /*pluperfect.erfect.ect Passive Indicative*/
+  output.indicative.pluperfect.passive = conj_with_est(" ", pluperfectEnds)
 
-  /*FutPerfect Active Indicative*/
-  FutPEnds = add_string_to_array("eri", ActEnds)
-  output.FutPActInd = join_parts(roots[2], FutPEnds);
-  output.FutPActInd[0] = roots[2] + "ero"; //1st sing is annoying
-  /*FutPerfect Passive Indicative*/
-  output.FutPPassInd = conj_with_est(" ", FutPEnds)
+  /*futurePerfect.erfect.ect Active Indicative*/
+  futureperfectEnds = add_string_to_array("eri", ActEnds)
+  output.indicative.futurePerfect.active = join_parts(roots[2], futureperfectEnds);
+  output.indicative.futurePerfect.active[0] = roots[2] + "ero"; //1st sing is annoying
+  /*futurePerfect.erfect.ect Passive Indicative*/
+  output.indicative.futurePerfect.passive = conj_with_est(" ", futureperfectEnds)
 
   /*Indicative*/
   /*page3*/
   roots[1] = givens[1];
 
-  /*Present Active Subjunctive*/
-  output.PresActSubj = join_parts(roots[0], "e", ActEnds);
-  /*Present Passive Subjunctive*/
-  output.PresPassSubj = join_parts(roots[0], "a", PassEnds);
+  /*present.ent Active Subjunctive*/
+  output.subjunctive.present.active = join_parts(roots[0], "e", ActEnds);
+  /*present.ent Passive Subjunctive*/
+  output.subjunctive.present.passive = join_parts(roots[0], "a", PassEnds);
 
-  /*Imperfect Active Subjunctive*/
-  output.ImpActSubj = join_parts(roots[1], ActEnds);
-  /*Imperfect Passive Subjunctive*/
-  output.ImpPassSubj = join_parts(roots[1], PassEnds);
+  /*imperfect.erfect Active Subjunctive*/
+  output.subjunctive.imperfect.active = join_parts(roots[1], ActEnds);
+  /*imperfect.erfect Passive Subjunctive*/
+  output.subjunctive.imperfect.passive = join_parts(roots[1], PassEnds);
 
 
-  /*Perfect Active Subjunctive*/
-  output.PerfActSubj = join_parts(roots[2], FutPEnds);
-  output.PerfActSubj[0] = roots[2] + "erem"; //1st sing is annoying
-  /*Perfect Passive Subjunctive*/
-  output.PerfPassSubj = conj_with_est(" si", ActEnds);
+  /*perfect.ect Active Subjunctive*/
+  output.subjunctive.perfect.active = join_parts(roots[2], futureperfectEnds);
+  output.subjunctive.perfect.active[0] = roots[2] + "erem"; //1st sing is annoying
+  /*perfect.ect Passive Subjunctive*/
+  output.subjunctive.perfect.passive = conj_with_est(" si", ActEnds);
 
-  /*Pluperfect Active Subjunctive*/
-  PlupEnds = add_string_to_array("isse", ActEnds);
-  output.PlupActSubj = join_parts(roots[2], PlupEnds);
-  /*Pluperfect Passive Subjunctive*/
-  output.PlupPassSubj = conj_with_est(" esse", ActEnds);
+  /*pluperfect.erfect.ect Active Subjunctive*/
+  pluperfectEnds = add_string_to_array("isse", ActEnds);
+  output.subjunctive.pluperfect.active = join_parts(roots[2], pluperfectEnds);
+  /*pluperfect.erfect.ect Passive Subjunctive*/
+  output.subjunctive.pluperfect.passive = conj_with_est(" esse", ActEnds);
 
   /*Output the stuffs*/
   txt = JSON.stringify(output, null, 2);
   txt = txt.replace(/\n/g, "<br/>")
   txt = createP(txt)
 }
+
+function roots_0_join() {}
 
 function join_parts(root, vowel, ending) {
   var vowel_array; //amazingly, we will need this.
