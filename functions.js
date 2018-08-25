7ecbcb39e70adaba894d7a50cf16ab1041da7cb2
Carlingr <kittyclubco@gmail.com>
2018-08-25 13:45:15 -0700

    Move functions to different file
    Moved functions other than setup to functions.js

---
+++ functions.js
@@ -0,0 +1,53 @@
+function join_parts(root, vowel, ending) {
+  var vowel_array; //amazingly, we will need this.
+  var output = ["", "", "", "", "", ""]; // 
+  if (ending === undefined) {
+    for (i = 0; i < 6; i++) { //deal with the regular rules
+      output[i] = root + vowel[i];
+    }
+  } else {
+    if (vowel.constructor !== Array) { //shit
+      vowel_array = new Array(6).fill(vowel); // make an array with all one value
+    } else { //vowel
+      vowel_array = vowel; //copypasta
+    }
+    for (i = 0; i < 6; i++) { //deal with the regular rules
+      output[i] = root + vowel_array[i] + ending[i];
+    }
+  }
+  return output;
+}
+
+function conj_with_est(middle, ending) {
+  var output = new Array(6);
+  for (i = 0; i < 3; i++) { //deal with the singular
+    output[i] = givens[3] + middle + ending[i]
+  }
+  for (i = 3; i < 6; i++) { //deal with the plurals
+    output[i] = roots[3] + middle + ending[i]
+  }
+  return output;
+}
+
+function add_string_to_array(add_this, to_this) {
+  var output = ["", "", "", "", "", ""];
+  for (i = 0; i < to_this.length; i++) { //deal with the regular rules
+    output[i] = add_this + to_this[i]
+  }
+  return output;
+}
+
+function ordinal_suffix_of(i) {
+  var j = i % 10,
+    k = i % 100;
+  if (j == 1 && k != 11) {
+    return i + "st";
+  }
+  if (j == 2 && k != 12) {
+    return i + "nd";
+  }
+  if (j == 3 && k != 13) {
+    return i + "rd";
+  }
+  return i + "th";
+}
